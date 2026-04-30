53b635d866299f0ee6082b508a93685225adf9c6
polyamsxa-hash <polyamsxa@gmail.com>
2026-04-30 03:24:24 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+from aiogram import Bot, Dispatcher, types
+import asyncio
+import requests
+
+BOT_TOKEN = "8769156866:AAE9oyETBI6HlDRboTzN4rDK6Dl2Y1GKVOU"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def ai_handler(message: types.Message):
+    try:
+        import urllib.parse
+        text = urllib.parse.quote(message.text)
+
+        url = f"https://text.pollinations.ai/{text}"
+        r = requests.get(url)
+
+        await message.answer(r.text)
+
+    except Exception as e:
+        print("❌ ОШИБКА:", e)
+        await message.answer("Ошибка 😔")
+
+async def main():
+    print("Бот запущен 🤖")
+    await dp.start_polling(bot)
+
+asyncio.run(main())
